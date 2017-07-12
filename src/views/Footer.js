73c4e6e01612248cf2492537b03a3d4b887b115b
Martin Nordholts <enselic@gmail.com>
2017-07-12 11:39:16 +0200

    Update version to 1.0.0
    We're using semver.org

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -6,7 +6,7 @@ export default function(props) {
     };
     return (
         <div style={{ padding: '10px 0px', background: 'black', position: 'absolute', top: '100%', width: '100%', fontSize: '80%', ...linkColor }}>
-            &nbsp;v0.9.0-beta1 @&nbsp;
+            &nbsp;v1.0.0 @&nbsp;
             <a style={{...linkColor}} target="_blank" rel="noopener noreferrer" href="https://github.com/Enselic/sequencediagram.io">GitHub</a> <br/>
             &nbsp;By&nbsp;<a style={{...linkColor}} target="_blank" rel="noopener noreferrer" href="https://setofskills.com/">setofskills.com</a> <br/>
             &nbsp;Twitter:&nbsp;<a style={{...linkColor}} target="_blank" rel="noopener noreferrer" href="https://twitter.com/SeqDiagram_io">@SeqDiagram_io</a> <br/>
