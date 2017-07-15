432307a98865759665b23ce03570b12a3db224d4
Martin Nordholts <enselic@gmail.com>
2017-07-15 10:12:30 +0200

    Make footer prettier

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -1,16 +1,33 @@
 import React from 'react'
 
 export default function(props) {
-    const linkColor = {
+    const leftRightPadding = 10;
+    const itemStyle = {
         color: '#ddd',
-    };
+        textDecoration: 'none',
+        paddingLeft: leftRightPadding,
+        paddingTop: 10,
+        marginRight: 30,
+    }
+    function ExternalLink(props) {
+        return <a style={{ ...itemStyle,  }} target="_blank" rel="noopener noreferrer" href={props.href}>{props.children}</a>
+    }
+
     return (
-        <div style={{ padding: '10px 0px', background: 'black', position: 'absolute', top: '100%', width: '100%', fontSize: '80%', ...linkColor }}>
-            &nbsp;v1.0.0 @&nbsp;
-            <a style={{...linkColor}} target="_blank" rel="noopener noreferrer" href="https://github.com/Enselic/sequencediagram.io">GitHub</a> <br/>
-            &nbsp;By&nbsp;<a style={{...linkColor}} target="_blank" rel="noopener noreferrer" href="https://setofskills.com/">setofskills.com</a> <br/>
-            &nbsp;Twitter:&nbsp;<a style={{...linkColor}} target="_blank" rel="noopener noreferrer" href="https://twitter.com/SeqDiagram_io">@SeqDiagram_io</a> <br/>
-            &nbsp;<a style={{...linkColor}} target="_blank" rel="noopener noreferrer" href="./acknowledgements.html">Acknowledgements</a>
+        <div style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                background: '#111',
+                position: 'absolute',
+                top: '100%',
+                width: '100%',
+                padding: '40px 0',
+                }}>
+            <ExternalLink href="https://github.com/Enselic/sequencediagram.io">GitHub</ExternalLink>
+            <ExternalLink href="https://setofskills.com">Blog</ExternalLink>
+            <ExternalLink href="https://twitter.com/SeqDiagram_io">Twitter</ExternalLink>
+            <ExternalLink href="./acknowledgements.html">Acknowledgements</ExternalLink>
+            <b style={{ marginLeft: 'auto', ...itemStyle, paddingRight: leftRightPadding }}>v1.1.0-beta</b>
         </div>
     );
 }
