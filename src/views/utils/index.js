7fa61556a3e055161235bef5bf65d95b7ac4be7f
Martin Nordholts <enselic@gmail.com>
2017-08-20 13:38:29 +0200

    src/views/utils/index.js: prettier
    Start porting files to use prettier.
    utils first out.
    
    Run like this:
    npm run prettier -- src/views/utils/index.js
    
    Eventually we will make the script apply to all files and add
    CI checks for it.

--- src/views/utils/index.js
+++ src/views/utils/index.js
@@ -1,85 +1,96 @@
-import * as ac from './../../reducers'
+import * as ac from "./../../reducers";
 
 export function hoverHelper(pending, dispatch, key) {
-    return {
-        onMouseEnter() {
-            dispatch(ac.hoverOverComponent(key));
-        },
-        onMouseMove() {
-            if (pending.hoveredComponentKey !== key) {
-                dispatch(ac.hoverOverComponent(key));
-            }
-        },
-        onMouseLeave() {
-            dispatch(ac.endHoverOverComponent());
-        }
+  return {
+    onMouseEnter() {
+      dispatch(ac.hoverOverComponent(key));
+    },
+    onMouseMove() {
+      if (pending.hoveredComponentKey !== key) {
+        dispatch(ac.hoverOverComponent(key));
+      }
+    },
+    onMouseLeave() {
+      dispatch(ac.endHoverOverComponent());
     }
+  };
 }
 
 export function isJsonDifferent(object1, object2) {
-    return JSON.stringify(object1) !== JSON.stringify(object2);
+  return JSON.stringify(object1) !== JSON.stringify(object2);
 }
 
 export function eventToDiagramCoords(event) {
-    const boundingClientRect = document.getElementById('diagram-root').getBoundingClientRect();
-    return [ event.clientX - boundingClientRect.left, event.clientY - boundingClientRect.top ];
+  const boundingClientRect = document
+    .getElementById("diagram-root")
+    .getBoundingClientRect();
+  return [
+    event.clientX - boundingClientRect.left,
+    event.clientY - boundingClientRect.top
+  ];
 }
 
 export function hoverLifelineHelper(dispatch, key) {
-    return {
-        onMouseEnter(e) {
-            dispatch(ac.mouseEnterLifeline(key, ...eventToDiagramCoords(e)));
-        },
-        onMouseMove(e) {
-            dispatch(ac.mouseEnterLifeline(key, ...eventToDiagramCoords(e)));
-        },
-        onMouseLeave() {
-            dispatch(ac.mouseLeaveLifeline());
-        }
+  return {
+    onMouseEnter(e) {
+      dispatch(ac.mouseEnterLifeline(key, ...eventToDiagramCoords(e)));
+    },
+    onMouseMove(e) {
+      dispatch(ac.mouseEnterLifeline(key, ...eventToDiagramCoords(e)));
+    },
+    onMouseLeave() {
+      dispatch(ac.mouseLeaveLifeline());
     }
+  };
 }
 
 export function transferPropsToStyle(object, style) {
-    for(let prop in object) {
-        if (object.hasOwnProperty(prop)) {
-            style[prop] = object[prop];
-        }
+  for (let prop in object) {
+    if (object.hasOwnProperty(prop)) {
+      style[prop] = object[prop];
     }
+  }
 }
 
 export function transferStyleToProps(style) {
-    return {
-        transition: style.transition,
-        left: style.left,
-        width: style.width,
-        top: style.top,
-        height: style.height,
-    };
+  return {
+    transition: style.transition,
+    left: style.left,
+    width: style.width,
+    top: style.top,
+    height: style.height
+  };
 }
 
 // Thanks Jonathan Marzullo and others from https://stackoverflow.com/a/13348618
 export function isChrome() {
   var isChromium = window.chrome,
-      winNav = window.navigator,
-      vendorName = winNav.vendor,
-      isOpera = winNav.userAgent.indexOf("OPR") > -1,
-      isIEedge = winNav.userAgent.indexOf("Edge") > -1,
-      isIOSChrome = winNav.userAgent.match("CriOS");
+    winNav = window.navigator,
+    vendorName = winNav.vendor,
+    isOpera = winNav.userAgent.indexOf("OPR") > -1,
+    isIEedge = winNav.userAgent.indexOf("Edge") > -1,
+    isIOSChrome = winNav.userAgent.match("CriOS");
 
-  if(isIOSChrome){
-      return true;
-  } else if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && !isOpera && !isIEedge) {
-      return true;
+  if (isIOSChrome) {
+    return true;
+  } else if (
+    isChromium !== null &&
+    isChromium !== undefined &&
+    vendorName === "Google Inc." &&
+    !isOpera &&
+    !isIEedge
+  ) {
+    return true;
   } else {
-      return false;
+    return false;
   }
 }
 
 export const eatMouseDown = {
-    onMouseDown: e => {
-        // We don't want the parent div to receive any mouse down event if
-        // this item is clicked
-        e.stopPropagation();
-        e.preventDefault();
-    }
-}
+  onMouseDown: e => {
+    // We don't want the parent div to receive any mouse down event if
+    // this item is clicked
+    e.stopPropagation();
+    e.preventDefault();
+  }
+};
