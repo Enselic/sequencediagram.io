95a179168c2d5c5722fcb57713aa9999df71071d
Martin Nordholts <enselic@gmail.com>
2017-09-24 10:28:28 +0200

    Don't tip about URL shorteners
    We will soon have our own solution, namely
    permalinks.

--- src/views/Header.js
+++ src/views/Header.js
@@ -44,20 +44,12 @@ export default function(props) {
         <div style={{ color: "#002456", padding: "12px 20px", paddingTop: 0 }}>
           <b>Share by URL:</b> The current diagram is tersely encoded in the
           current URL and can simply be shared as-is by copy-paste from the
-          browser address bar. You might want to make use of{" "}
-          <a
-            style={{ padding: 0 }}
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.google.se/search?q=url+shortener"
-          >
-            URL shortener services
-          </a>.
+          browser address bar.
         </div>
       )}
 
       {showShareInfo && (
-        <div style={{ color: "#fff", padding: "12px 10px", paddingTop: 0 }}>
+        <div style={{ color: "#002456", padding: "12px 20px", paddingTop: 0 }}>
           <b>Share by PNG:</b> <Kbd>F12</Kbd> (for <i>Developer Tools</i>) then{" "}
           <Kbd>Ctrl/Cmd + Shift + P</Kbd> (for <i>Command Menu</i>) then{" "}
           <Kbd>Capture full size screenshot</Kbd>.
@@ -65,7 +57,7 @@ export default function(props) {
       )}
 
       {showNewContentAvailable && (
-        <div style={{ color: "#fff", padding: "12px 10px", paddingTop: 0 }}>
+        <div style={{ color: "#002456", padding: "12px 20px", paddingTop: 0 }}>
           <b>New version available</b> A new version of the this web app has
           been published. Press F5 to load it.
         </div>
