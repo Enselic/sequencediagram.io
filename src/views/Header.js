4d5dca709c41950b4048e02ef34634428e7ee497
Martin Nordholts <enselic@gmail.com>
2017-10-03 19:52:33 +0200

    Warn if using git-master branch

--- src/views/Header.js
+++ src/views/Header.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { isChrome } from "./utils";
 
+const gitMasterBuild = document.location.host.indexOf("git-master") !== -1;
+
+function Kbd(props) {
+  return (
+    <kbd
+      style={{
+        display: "inline-block",
+        padding: "0.2em",
+        color: "black",
+        backgroundColor: "#ccc",
+        borderRadius: "4px",
+      }}
+    >
+      {props.children}
+    </kbd>
+  );
+}
+
+function Info(props) {
+  return (
+    <div style={{ color: "#002456", padding: "10px 20px", paddingTop: 0 }}>
+      {props.children}
+    </div>
+  );
+}
+
 export default function(props) {
   const { showShareInfo, touchWarn, showNewContentAvailable } = props.pending;
-
-  function Kbd(props) {
-    return (
-      <kbd
-        style={{
-          display: "inline-block",
-          padding: "0.2em",
-          color: "black",
-          backgroundColor: "#ccc",
-          borderRadius: "4px",
-        }}
-      >
-        {props.children}
-      </kbd>
-    );
-  }
 
   return (
     <div
@@ -26,41 +36,49 @@ export default function(props) {
         backgroundColor: "#64b9ef",
       }}
     >
+      {gitMasterBuild && (
+        <Info>
+          <b>Risk of dataloss:</b>
+          <br />This is a build deployed by continous integration directly from
+          the git master branch
+        </Info>
+      )}
+
       {touchWarn && (
-        <div style={{ color: "#002456", padding: "10px 20px", paddingTop: 0 }}>
-          Touch input is not supported yet, please use a mouse or contribute
-          touch input support via GitHub
-        </div>
+        <Info>
+          <b>Touch input is not supported:</b> Touch input is not supported yet,
+          please use a mouse or contribute touch input support via GitHub
+        </Info>
       )}
 
       {!isChrome() && (
-        <div style={{ color: "#002456", padding: "10px 20px", paddingTop: 0 }}>
-          Your browser is not supported yet, please use Google Chrome or
-          contribute more browser support via GitHub
-        </div>
+        <Info>
+          <b>Unsupported browser:</b> Your browser is not supported yet, please
+          use Google Chrome or contribute more browser support via GitHub
+        </Info>
       )}
 
       {showShareInfo && (
-        <div style={{ color: "#002456", padding: "12px 20px", paddingTop: 0 }}>
+        <Info>
           <b>Share by URL:</b> The current diagram is tersely encoded in the
           current URL and can simply be shared as-is by copy-paste from the
           browser address bar.
-        </div>
+        </Info>
       )}
 
       {showShareInfo && (
-        <div style={{ color: "#002456", padding: "12px 20px", paddingTop: 0 }}>
+        <Info>
           <b>Share by PNG:</b> <Kbd>F12</Kbd> (for <i>Developer Tools</i>) then{" "}
           <Kbd>Ctrl/Cmd + Shift + P</Kbd> (for <i>Command Menu</i>) then{" "}
           <Kbd>Capture full size screenshot</Kbd>.
-        </div>
+        </Info>
       )}
 
       {showNewContentAvailable && (
-        <div style={{ color: "#002456", padding: "12px 20px", paddingTop: 0 }}>
-          <b>New version available</b> A new version of the this web app has
+        <Info>
+          <b>New version available:</b> A new version of the this web app has
           been published. Press F5 to load it.
-        </div>
+        </Info>
       )}
     </div>
   );
