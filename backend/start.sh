fe27d0fe8dfcd6352d167b54e22bc4ff97bb9f8f
Martin Nordholts <enselic@gmail.com>
2017-10-05 21:01:36 +0200

    Add npm run start-local
    Instead of backend/start.sh.
    And make npm start start dynamodb-local
    so npm test pass effortlessly.

--- backend/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-trap 'kill -TERM 0' EXIT
-
-node backend/dynamodb-local.js &
-node backend/api-server-local.js &
-
-# Wait for Ctrl-C
-cat
---
+++ scripts/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+trap 'kill -TERM 0' EXIT
+
+# By default, start what is needed for npm test to pass all tests
+node backend/dynamodb-local.js &
+node_modules/.bin/react-scripts start &
+
+# If an argument is given, also start the local API server
+# This will make some tests fail (since they need to be able)
+# to start and stop the server manually), but it allows you
+# to develop against local services only
+[ -z "$1" ] || node backend/api-server-local.js &
+
+# Wait for Ctrl-C
+cat
