Enselic/sequencediagram.io

commit 9be3c72c07e78bd11273e8dad358b2c769c4e9e0

Use App.state instead of Redux state for move
We do this for several reasons:
* It fixes messsage arrows not updating when changing
  their directions by moving the object the are attached
  to. This bug was introduced when we started to use
  SVG for arrows
* Redux does not work well for UI state. The code becomes
  simpler by treating move state as App-internal React state.
  This in turn makes it easier to add features such as object
  creation and destruction visualization.

The code could look better, but I will wait with refactoring.
Right now I just want to get this change in. All end-to-end
tests pass of course.

src/views/App.js
@@ -5,7 +5,11 @@ import Message from "./Message";
 import Header from "./Header";
 import Menu from "./Menu";
 import NewMessageMarker from "./NewMessageMarker";
-import { eventToDiagramCoords, mapWithSameDomOrder } from "./utils";
+import {
+  eventToDiagramCoords,
+  mapWithSameDomOrder,
+  isJsonDifferent,
+} from "./utils";
 import * as ac from "./../reducers";
 
 export default class App extends React.Component {
@@ -16,6 +20,7 @@ export default class App extends React.Component {
 
     this.state = {
       messageAnchorMoved: undefined,
+      componentMoved: undefined,
     };
 
     this.handleOnAnchorClick = (message, type, newX) => {
@@ -26,6 +31,99 @@ export default class App extends React.Component {
       if (e.keyCode === 27) {
         this.setState({ messageAnchorMoved: undefined });
       }
+    };
+
+    this.handleComponentMouseDown = (e, movedComponent) => {
+      if (
+        this.props.state.pending.componentRenamed &&
+        this.props.state.pending.componentRenamed.key === movedComponent.key
+      ) {
+        // The move helper eats mouse events, causing e.g. "click in text to place cursor" to break, so
+        // disable the move helper if this object is being renamed
+        return;
+      }
+
+      const thiz = this;
+      const isMovingObject = movedComponent.id[0] === "o";
+
+      const eventToPos = isMovingObject ? e => e.pageX : e => e.pageY;
+
+      const downPos = eventToPos(e);
+      let movedAwayFromClick = false;
+
+      const idToStylePos = id => {
+        const style = document.getElementById(id).style;
+        return parseInt(isMovingObject ? style.left : style.top, 10);
+      };
+      let grabOffset = isMovingObject
+        ? downPos - idToStylePos(movedComponent.id)
+        : e.nativeEvent.offsetY + 10 /* message name background border */;
+
+      const { objects, messages } = this.props.state.core.present;
+      const components = isMovingObject ? objects : messages;
+      let pendingComponents = components.map(value => {
+        return { ...value };
+      });
+
+      function updatePositions(e) {
+        const diagramCoords = eventToDiagramCoords(e);
+        const offsettedPos =
+          (isMovingObject ? diagramCoords[0] : diagramCoords[1]) - grabOffset;
+
+        pendingComponents.sort(function(o1, o2) {
+          function toX(component) {
+            if (component.id === movedComponent.id) {
+              return offsettedPos;
+            } else {
+              return idToStylePos(component.id);
+            }
+          }
+
+          return toX(o1) - toX(o2);
+        });
+
+        thiz.setState({
+          componentMoved: { component: movedComponent, newPos: offsettedPos },
+          pendingComponents,
+        });
+
+        const newPos = eventToPos(e);
+        if (Math.abs(newPos - downPos) > 20) {
+          movedAwayFromClick = true;
+        }
+      }
+
+      function mousemove(e) {
+        e.preventDefault();
+
+        updatePositions(e);
+      }
+      window.addEventListener("mousemove", mousemove);
+
+      function mouseup(e) {
+        e.preventDefault();
+
+        if (isJsonDifferent(pendingComponents, components)) {
+          const fn = isMovingObject
+            ? ac.rearrangeObjects
+            : ac.rearrangeMessages;
+          thiz.props.dispatch(fn(pendingComponents));
+        }
+
+        window.removeEventListener("mousemove", mousemove);
+
+        thiz.setState({
+          componentMoved: undefined,
+          pendingComponents: undefined,
+        });
+
+        if (!movedAwayFromClick) {
+          thiz.props.dispatch(
+            ac.editComponentName(movedComponent.id, movedComponent.name, true)
+          );
+        }
+      }
+      window.addEventListener("mouseup", mouseup, { once: true });
     };
   }
 
@@ -44,7 +142,7 @@ export default class App extends React.Component {
     // We want to perform the layout as if the pending names were commited, so that
     // the diagram layout adapts to the pending names which we show as part of the
     // diagram
-    function withPendingNewName(component) {
+    function withPendingChanges(component) {
       if (
         pending.componentRenamed &&
         pending.componentRenamed.id === component.id
@@ -58,16 +156,43 @@ export default class App extends React.Component {
         newComponent[thiz.state.messageAnchorMoved.type] =
           thiz.state.messageAnchorMoved.newX;
         return newComponent;
+      } else if (thiz.state.componentMoved) {
+        const theMovedComponent = thiz.state.componentMoved.component;
+        const newComponent = { ...component };
+        const newPos = thiz.state.componentMoved.newPos;
+        if (theMovedComponent.id === component.id) {
+          if (component.id[0] === "o") {
+            newComponent.overrideLifelineX = newPos;
+          } else {
+            newComponent.overrideTop = newPos;
+          }
+        } else if (
+          theMovedComponent.id === component.sender ||
+          theMovedComponent.id === component.receiver
+        ) {
+          newComponent.overrideNoTransition = true;
+        }
+
+        return newComponent;
       } else {
         return component;
       }
     }
-    const objectsWithPendingNames = objects.map(withPendingNewName);
-    const messagesWithPendingNames = messages.map(withPendingNewName);
+    let objectsToUse = objects;
+    let messagesToUse = messages;
+    if (this.state.componentMoved) {
+      if (this.state.componentMoved.component.id[0] === "o") {
+        objectsToUse = this.state.pendingComponents;
+      } else {
+        messagesToUse = this.state.pendingComponents;
+      }
+    }
+    const objectsWithPendingChanges = objectsToUse.map(withPendingChanges);
+    const messagesWithPendingChanges = messagesToUse.map(withPendingChanges);
     const layout = layouter(
       name => name.length * 7 /* TODO: hack */,
-      objectsWithPendingNames,
-      messagesWithPendingNames,
+      objectsWithPendingChanges,
+      messagesWithPendingChanges,
       pending.message
     );
     const usefulProps = { objects, messages, pending, dispatch, layout };
@@ -132,8 +257,10 @@ export default class App extends React.Component {
 
     const showControls =
       !pending.message &&
-      !(pending.componentMoved && pending.componentMoved.part) &&
-      !thiz.state.messageAnchorMoved;
+      !(
+        this.state.componentMoved && this.state.componentMoved.component.part
+      ) &&
+      !this.state.messageAnchorMoved;
 
     return (
       <div
@@ -164,6 +291,7 @@ export default class App extends React.Component {
               object={object}
               onLifelineClick={handleLifelineClick(object)}
               onRemove={() => dispatch(ac.removeComponent(object.id))}
+              onComponentMouseDown={this.handleComponentMouseDown}
               showControls={showControls}
               {...usefulProps}
             />
@@ -178,6 +306,7 @@ export default class App extends React.Component {
                 msgLayout={msgLayout}
                 onAnchorClick={this.handleOnAnchorClick}
                 onRemove={() => dispatch(ac.removeComponent(message.id))}
+                onComponentMouseDown={this.handleComponentMouseDown}
                 showControls={showControls}
                 isPending={
                   this.state.messageAnchorMoved &&
@@ -199,8 +328,8 @@ export default class App extends React.Component {
           )}
 
           {pending.lifelineHoveredKey &&
-          (!pending.componentMoved ||
-            pending.componentMoved.part ||
+          (!this.state.componentMoved ||
+            this.state.componentMoved.component.part ||
             this.state.messageAnchorMoved) && (
             <NewMessageMarker
               left={layout[pending.lifelineHoveredKey].lifelineX}
@@ -213,7 +342,10 @@ export default class App extends React.Component {
               }
               isStart={
                 !!!pending.message &&
-                !(pending.componentMoved && pending.componentMoved.part) &&
+                !(
+                  this.state.componentMoved &&
+                  this.state.componentMoved.component.part
+                ) &&
                 !this.state.messageAnchorMoved
               }
               direction={

src/layouter.js
@@ -54,6 +54,10 @@ export function layoutMessageLeftAndWidth(
     receiverX = receiver.lifelineX;
   }
 
+  if (message.overrideNoTransition) {
+    transition = null;
+  }
+
   let direction = receiverX - senderX;
 
   let left;
@@ -95,11 +99,19 @@ export function layoutMessageLeftAndWidth(
 }
 
 function layoutObject(getTextWidth, currentX, object) {
+  let transition = devUtils.transitionIfNotDev("left 0.3s");
   const objectNameWidth =
     getTextWidth(object.name) + OBJECT_NAME_PADDING.LEFT_RIGHT;
+  let lifelineX = currentX + objectNameWidth / 2;
+
+  if (object.overrideLifelineX) {
+    transition = "none";
+    lifelineX = object.overrideLifelineX;
+  }
   const objectLayout = {
-    lifelineX: currentX + objectNameWidth / 2,
+    lifelineX,
     top: DIAGRAM_PADDING.TOP_BOTTOM,
+    transition,
   };
   const newX = currentX + objectNameWidth + OBJECT_SPACING;
   return { newX, objectLayout };
@@ -171,7 +183,7 @@ export default function(getTextWidth, objects, messages, extraMessage) {
 
     layout[message.id] = {
       ...leftRightValues,
-      top: currentY,
+      top: message.overrideTop ? message.overrideTop : currentY,
     };
 
     currentY += MESSAGE_SPACING + leftRightValues.approximateHeight;

src/reducers/pending.js
@@ -9,18 +9,6 @@ export function pendingAddMessage(sender, x, y, name) {
   return { type: "PENDING_ADD_MESSAGE", sender, x, y, name };
 }
 
-/**
- * @param id Key of component. Message or Object.
- * @param part Part of message. "sender" or "receiver". If not specified, entire component is moved.
- */
-export function beginComponentMove(id, part) {
-  return { type: "BEGIN_COMPONENT_MOVE", id, part };
-}
-
-export function endComponentMove(id) {
-  return { type: "END_COMPONENT_MOVE" };
-}
-
 export function editComponentName(id, newName, preselect) {
   return {
     type: "EDIT_COMPONENT_NAME",
@@ -88,13 +76,6 @@ export default function(state = {}, action) {
             ? undefined
             : state.message,
       };
-    case "BEGIN_COMPONENT_MOVE":
-      return {
-        ...state,
-        componentMoved: { id: action.id, part: action.part },
-      };
-    case "END_COMPONENT_MOVE":
-      return { ...state, componentMoved: undefined };
     case "EDIT_COMPONENT_NAME":
       return {
         ...state,

src/views/Message.js
@@ -1,6 +1,5 @@
 import * as ac from "./../reducers";
 import React from "react";
-import moveHelper from "./utils/componentMoveHelper";
 import Name from "./Name";
 import MessageArrow from "./MessageArrow";
 import { hoverHelper } from "./utils";
@@ -10,29 +9,20 @@ function Message(props) {
     dispatch,
     message,
     msgLayout,
-    objects,
-    messages,
     pending,
     isPending,
     isMarker,
+    onComponentMouseDown,
   } = props;
 
-  let onMouseDown;
+  const handleMouseDown = e => {
+    if (onComponentMouseDown) {
+      onComponentMouseDown(e, message);
+    }
+  };
+
   let style;
   if (!isPending) {
-    onMouseDown = moveHelper(
-      objects,
-      messages,
-      message,
-      e => e.pageY,
-      el => el.style.top,
-      ac.beginComponentMove,
-      ac.endComponentMove,
-      ac.rearrangeMessages,
-      dispatch,
-      pending
-    );
-
     style = {
       pointerEvents: pending.componentMoved ? "none" : "auto",
     };
@@ -51,7 +41,7 @@ function Message(props) {
 
   return (
     <div
-      onMouseDown={onMouseDown}
+      onMouseDown={handleMouseDown}
       style={{ ...msgLayout, ...style }}
       id={message.id}
       key={message.id}

src/views/Object.js
@@ -1,9 +1,6 @@
-import * as ac from "./../reducers";
 import React from "react";
-import moveHelper from "./utils/componentMoveHelper";
 import Name from "./Name";
 import RemoveButton from "./RemoveButton";
-import devUtils from "./../devUtils";
 import {
   OBJECT_NAME_PADDING,
   OBJECT_NAME_FONT_SIZE_PX,
@@ -21,33 +18,25 @@ function DiagramObject(props) {
     dispatch,
     object,
     layout,
-    objects,
-    messages,
     pending,
     isHovered,
     onLifelineClick,
+    onComponentMouseDown,
     showControls,
     onRemove,
   } = props;
 
-  const onMouseDown = moveHelper(
-    objects,
-    messages,
-    object,
-    e => e.pageX,
-    el => el.style.left,
-    ac.beginComponentMove,
-    ac.endComponentMove,
-    ac.rearrangeObjects,
-    dispatch,
-    pending
-  );
+  const handleMouseDown = e => {
+    if (onComponentMouseDown) {
+      onComponentMouseDown(e, object);
+    }
+  };
 
   let style = {
     left: layout[object.id].lifelineX,
     top: layout[object.id].top,
+    transition: layout[object.id].transition,
     position: "absolute",
-    transition: devUtils.transitionIfNotDev("left 0.3s"),
     userSelect: "none",
   };
 
@@ -76,7 +65,7 @@ function DiagramObject(props) {
           />
         )}
         <div
-          onMouseDown={onMouseDown}
+          onMouseDown={handleMouseDown}
           style={{
             background: "#ffe761",
             padding: `${OBJECT_NAME_PADDING.TOP_BOTTOM}px ${OBJECT_NAME_PADDING.LEFT_RIGHT}px`,

src/views/utils/componentMoveHelper.js
@@ -1,177 +1,0 @@
-import * as ac from "./../../reducers";
-import layouter, { layoutMessageLeftAndWidth } from "./../../layouter";
-import { transferPropsToStyle, transferStyleToProps, isJsonDifferent } from ".";
-
-/* To get high FPS while moving things around, manipulate DOM objects directly */
-export default function(
-  objects,
-  messages,
-  movedComponent,
-  eventToPos,
-  elementToPos,
-  beginComponentMove,
-  endComponentMove,
-  rearrangeComponents,
-  dispatch,
-  pending
-) {
-  // The move helper eats mouse events, causing e.g. "click in text to place cursor" to break, so
-  // disable the move helper if this object is being renamed
-  const beingRenamed =
-    pending.componentRenamed &&
-    pending.componentRenamed.id === movedComponent.id;
-
-  return beingRenamed
-    ? null
-    : e => {
-        const downPos = eventToPos(e);
-        let grabOffset = 0;
-        let movedAwayFromClick = false;
-
-        function getUiComponents(component) {
-          const el = document.getElementById(component.id);
-
-          if (component.id === movedComponent.id) {
-            grabOffset = eventToPos(e) - parseInt(elementToPos(el), 10);
-          }
-
-          const style = el.style;
-          return {
-            component: component,
-            el: el,
-            oldStyle: transferStyleToProps(style),
-          };
-        }
-
-        const uiObjects = objects.map(getUiComponents);
-        const uiMessages = messages.map(getUiComponents);
-
-        const isMovingObject = movedComponent.id[0] === "o";
-
-        const uiComponents = isMovingObject ? uiObjects : uiMessages;
-        const components = isMovingObject ? objects : messages;
-
-        function updatePositions(e) {
-          const offsettedPos = eventToPos(e) - grabOffset;
-
-          const newPos = eventToPos(e);
-          if (Math.abs(newPos - downPos) > 20) {
-            movedAwayFromClick = true;
-          }
-
-          uiComponents.sort(function(o1, o2) {
-            function toX(uiComponent) {
-              if (uiComponent.component.id === movedComponent.id) {
-                return offsettedPos;
-              } else {
-                return parseInt(elementToPos(uiComponent.el), 10);
-              }
-            }
-
-            return toX(o1) - toX(o2);
-          });
-          const layout = layouter(
-            name => name.length * 7 /* TODO: hack */,
-            uiObjects.map(uiComponent => uiComponent.component),
-            uiMessages.map(uiComponent => uiComponent.component)
-          );
-
-          uiObjects.forEach((uiObject, index) => {
-            let pos;
-            let transition;
-            if (uiObject.component.id === movedComponent.id) {
-              pos = offsettedPos;
-              transition = undefined;
-            } else {
-              pos = layout[uiObject.component.id].lifelineX;
-              transition = uiObject.oldStyle.transition;
-            }
-            const style = uiObject.el.style;
-            uiObject.el.style.left = pos + "px";
-            style.transition = transition;
-          });
-          uiMessages.forEach((uiMessage, index) => {
-            const l = layout[uiMessage.component.id];
-            let pos = l.top;
-            let left = l.left;
-            let width = l.width;
-            let borderWidth = l.borderWidth;
-            let borderImage = l.borderImage;
-            let transition = uiMessage.oldStyle.transition;
-            const o = uiMessage.component;
-            if (o.id === movedComponent.id) {
-              pos = offsettedPos;
-              transition = undefined;
-            }
-            if (
-              (o.sender && o.sender === movedComponent.id) ||
-              (o.receiver && o.receiver === movedComponent.id)
-            ) {
-              transition = undefined;
-            }
-
-            const movingSender = o.sender === movedComponent.id;
-            const movingReceiver = o.receiver === movedComponent.id;
-            if (isMovingObject && (movingSender || movingReceiver)) {
-              const tmpLayout = layoutMessageLeftAndWidth(
-                layout,
-                uiMessage.component,
-                movingSender ? offsettedPos : undefined,
-                movingReceiver ? offsettedPos : undefined
-              );
-              left = tmpLayout.left;
-              width = tmpLayout.width;
-              borderWidth = tmpLayout.borderWidth;
-              borderImage = tmpLayout.borderImage;
-            }
-            const style = uiMessage.el.style;
-            style.transition = transition;
-            style.top = pos + "px";
-            style.left = left + "px";
-            style.width = width + "px";
-            style.borderWidth = borderWidth;
-            style.borderImage = borderImage;
-          });
-        }
-
-        function mousemove(e) {
-          e.preventDefault();
-
-          updatePositions(e);
-        }
-        window.addEventListener("mousemove", mousemove);
-
-        function mouseup(e) {
-          e.preventDefault();
-
-          // Reset CSS and collect new arrays of domain components
-          function reduceAndReset(newComponents, uiComponent) {
-            transferPropsToStyle(uiComponent.oldStyle, uiComponent.el.style);
-
-            newComponents.push(uiComponent.component);
-            return newComponents;
-          }
-          let newObjects = uiObjects.reduce(reduceAndReset, []);
-          let newMessages = uiMessages.reduce(reduceAndReset, []);
-          const newComponents = isMovingObject ? newObjects : newMessages;
-
-          if (isJsonDifferent(newComponents, components)) {
-            dispatch(rearrangeComponents(newComponents));
-          }
-
-          window.removeEventListener("mousemove", mousemove);
-          dispatch(endComponentMove());
-
-          if (!movedAwayFromClick) {
-            dispatch(
-              ac.editComponentName(movedComponent.id, movedComponent.name, true)
-            );
-          }
-        }
-        window.addEventListener("mouseup", mouseup, { once: true });
-
-        updatePositions(e);
-
-        dispatch(beginComponentMove(movedComponent.id));
-      };
-}

commit 1a0c6f3a4fcb708cb21ba9d52a048481e7c22dbe

Don't show controls while moving components
This fixes a regression introduced with

  commit 9be3c72c07e78bd11273e8dad358b2c769c4e9e0
  Use App.state instead of Redux state for move

src/views/App.js
@@ -258,9 +258,7 @@ export default class App extends React.Component {
 
     const showControls =
       !pending.message &&
-      !(
-        this.state.componentMoved && this.state.componentMoved.component.part
-      ) &&
+      !this.state.componentMoved &&
       !this.state.messageAnchorMoved;
 
     return (