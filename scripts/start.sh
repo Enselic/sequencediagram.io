05adad5712494b8d1ebceec85fcdf161004cbc97
Martin Nordholts <enselic@gmail.com>
2017-10-07 22:12:38 +0200

    Use $(git describe) for version
    This will make it easier to automated deployments from CI.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,22 @@
-#!/bin/bash
+#!/bin/bash -e
 
 trap 'kill -TERM 0' EXIT
 
-# By default, start what is needed for npm test to pass all tests
-node backend/dynamodb-local.js &
-node_modules/.bin/react-scripts start &
+export REACT_APP_VERSION=$(git describe)
 
-# If an argument is given, also start the local API server
-# This will make some tests fail (since they need to be able)
-# to start and stop the server manually), but it allows you
-# to develop against local services only
-[ -z "$1" ] || node backend/api-server-local.js &
+if [ "$1" = "build" ]; then
+  node_modules/.bin/react-scripts build
+else
+  # By default, start what is needed for npm test to pass all tests
+  node backend/dynamodb-local.js &
+  node_modules/.bin/react-scripts start &
 
-# Wait for Ctrl-C
-cat
+  # If an argument is given, also start the local API server
+  # This will make some tests fail (since they need to be able)
+  # to start and stop the server manually), but it allows you
+  # to develop against local services only
+  [ -z "$1"] || node backend/api-server-local.js &
+
+  # Wait for Ctrl-C
+  cat
+fi
