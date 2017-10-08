a669c7845088419b74335976da02aba261296aa1
Martin Nordholts <enselic@gmail.com>
2017-10-08 12:58:36 +0200

    scripts/start.sh -> scripts.sh
    So we can rebase master-with-code-coverage with less hassle.

--- src/end-to-end-test/sanity.test.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-
-it(".travis.yml is present", async () => {
-  return fs.readFileSync(".travis.yml");
-});
---
+++ src/end-to-end-test/source-code-sanity.test.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+// It is useful to remove .travis.yml for work-in-progress
+// branches, but make sure we don't forget to restore it
+it(".travis.yml is present", async () => {
+  return fs.readFileSync(".travis.yml");
+});
+
+// When we want to rebase the code coverage branch by doing
+// a fresh `npm run eject`, having a scripts/ dir will trigger
+// an error, so we have to live without it
+it("scripts/ is not present", async () => {
+  let success;
+  try {
+    fs.statSync("scripts").isDirectory();
+    success = false;
+  } catch (e) {
+    success = true;
+  }
+  expect(success).toBeTruthy();
+});
