01a54b39cf5f94aa438b533076486802d858761a
Martin Nordholts <enselic@gmail.com>
2017-12-13 12:27:03 +0100

    scripts-ci/wait-for-ports.sh: --noproxy localhost
    Otherwise the command will succeed for all ports when a proxy
    is used.

--- scripts-ci/wait-for-ports.sh
+++ scripts-ci/wait-for-ports.sh
@@ -9,7 +9,7 @@ seconds=0
 while [ $# -gt 0 -a $seconds -lt $TIMEOUT_IN_SECONDS ]; do
 
   port=$1
-  if curl localhost:$port >/dev/null 2>&1; then
+  if curl --noproxy localhost localhost:$port >/dev/null 2>&1; then
      echo "Port $port is up (waited for $seconds seconds)"
      shift
      continue
