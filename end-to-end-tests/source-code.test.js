6eb3873b4f1e3c06aa433602df8821f7914eb47c
Martin Nordholts <enselic@gmail.com>
2017-12-21 20:18:09 +0100

    Partly eject react-scripts 1.0.14: scripts/build.js
    This allows us to add code coverage to the app build in
    TravisCI while still keeping the goodies of other react-scripts
    parts without bloating our repositoriy.

--- end-to-end-tests/source-code.test.js
+++ end-to-end-tests/source-code.test.js
@@ -9,23 +9,11 @@ it('.travis.yml is present', async () => {
   return fs.readFileSync('.travis.yml');
 });
 
-// When we want to rebase the code coverage branch by doing
-// a fresh `npm run eject`, having a scripts/ dir will trigger
-// an error, so we have to live without it
-it('scripts/ is not present', async () => {
-  let success;
-  try {
-    fs.statSync('scripts').isDirectory();
-    success = false;
-  } catch (e) {
-    success = true;
-  }
-  expect(success).toBeTruthy();
-});
-
 it('is formated with prettier', () => {
-  const files = glob.sync('{backend,end-to-end-tests,src}/**/*.js');
-  expect(files.length).toBeGreaterThan(39); // Sanity check
+  const files = glob.sync(
+    '{backend,config,end-to-end-tests,scripts,src}/**/*.js'
+  );
+  expect(files.length).toBeGreaterThan(48); // Sanity check
 
   const notProperlyFormated = [];
   files.forEach(file => {
