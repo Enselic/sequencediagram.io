0179df10d901c8c845a3441cbe292426be44c655
Martin Nordholts <enselic@gmail.com>
2018-03-06 09:26:47 +0100

    wip

--- end-to-end-tests/misc-object.test.py
+++ end-to-end-tests/misc-object.test.py
@@ -5,9 +5,19 @@ from selenium.webdriver.support import expected_conditions as EC # available sin
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import json
+import unittest
 
-# Create a new instance of the Firefox driver
-driver = webdriver.Firefox()
+class MiscObjectTestCase(unittest.TestCase):
+    def setUp(self):
+        driver = webdriver.Firefox()
+
+    def change_object_name(self):
+        start_with(driver, {"objects":[{"id":"o1","name":"ChangeMyName"}],"messages":[]})
+        rename_from_to(driver, "ChangeMyName", "NewText")
+        assert 1 == 1, 'incorrect default size'
+
+    def tearDown(self):
+        driver.quit()
 
 def find_element_by_partial_text(driver, text):
     return driver.find_element_by_xpath("//*[contains(text(),'" + text + "')]")
@@ -28,23 +38,6 @@ def start_with(driver, sequence_diagram):
     script = "return window.sequencediagram_io.setCurrentDiagram('" + json.dumps(sequence_diagram) + "');"
     driver.execute_script(script)
 
+if __name__ == '__main__':
+    unittest.main()
 
-
-# go to the google home page
-start_with(driver, {"objects":[{"id":"o1","name":"ChangeMyName"}],"messages":[]})
-rename_from_to(driver, "ChangeMyName", "NewText")
-click_text(driver, )
-
-
-
-
-
-try:
-    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
-
-    # You should see "cheese! - Google Search"
-    print driver.title
-
-finally:
-    driver.quit()
